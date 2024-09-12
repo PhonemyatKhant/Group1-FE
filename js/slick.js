e27840127c53e07345c8ac2d13bd5ff427fe7770
ZawYeHtike02 <zawyeye17502@gmail.com>
2024-09-12 16:17:42 +0630

    tablet-responsive-but slider it's no ok

--- js/slick.js
+++ js/slick.js
@@ -1,2 +1,10 @@
 $(document).ready(function(){
-});
+    $('.slider-wrapper').slick({
+        dots: true,
+        // arrows: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+    });
